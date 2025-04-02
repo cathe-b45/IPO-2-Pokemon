7f01a815e9e0e24a048ab4f574628066279d7cfe
Catherine Ballester <75802344+cathe-b45@users.noreply.github.com>
2025-04-02 22:05:24 +0200

    Proyecto Pokemon

---
+++ ProyectoUWP Catherine Ballester/ProyectoUWPenBlanco/MainPage.xaml.cs
@@ -0,0 +1,161 @@
+﻿using ControlDeUsuarioPokemon;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace ProyectoUWPenBlanco
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void cambiarVida(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            this.Mimikyu.Vida = e.NewValue;
+        }
+
+        private void cambiarEnergía(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            this.Mimikyu.energia = e.NewValue;
+        }
+
+        private void EjecutarAtaqueFuerte(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.animacionAtaqueFuerte();
+        }
+
+        private void EjecutarAtaqueFlojo(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.animacionAtaqueFlojo();
+        }
+
+        private void EjecutarDefensa(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.animacionDefensa();
+        }
+
+        private void EjecutarDescansar(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.animacionDescasar();
+        }
+
+        private void activarIddle(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.activarAniIdle(true);
+        }
+
+        private void desactivarIddle(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.activarAniIdle(false);
+        }
+
+        private void desactivarCansado(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.animacionNoCansado();
+        }
+
+        private void activarCansado(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.animacionCansado();
+        }
+
+        private void activarHerido(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.animacionHerido();
+        }
+
+        private void desactivarHerido(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.animacionNoHerido();
+        }
+
+        private void desactivarEscudo(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.verEscudo(false);
+        }
+
+        private void activarEscudo(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.verEscudo(true);
+        }
+
+        private void verFondo(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.verFondo(true);
+        }
+
+        private void noVerFondo(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.verFondo(false);
+        }
+
+        private void noVerFilaVida(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.verFilaVida(false);
+        }
+
+        private void verFilaVida(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.verFilaVida(true);
+        }
+
+        private void verFilaEnergia(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.verFilaEnergia(true);
+        }
+
+        private void noVerFilaEnergía(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.verFilaEnergia(false);
+        }
+
+        private void verPocimaVida(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.verPocionVida(true);
+        }
+
+        private void noVerPocimaVida(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.verPocionVida(false);
+        }
+
+        private void noVerPocimaEnergia(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.verPocionEnergia(false);
+        }
+
+        private void verPocimaEnergia(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.verPocionEnergia(true);
+        }
+
+        private void verNombrePokemon(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.verNombre(true);
+        }
+
+        private void noVerNombrePokemon(object sender, RoutedEventArgs e)
+        {
+            Mimikyu.verNombre(false);
+        }
+    }
+}
---
+++ ProyectoUWP Catherine Ballester/ProyectoUWPenBlanco/MimikyuCBM.xaml.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Control de usuario está documentada en https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace ControlDeUsuarioPokemon
+{
+    public sealed partial class MimikyuCBM : UserControl, iPokemon
+    {
+        DispatcherTimer dtTimeVida;
+        DispatcherTimer dtTimeEergia;
+
+        public double Vida
+        {
+            get { return this.BarraVida.Value; }
+            set { this.BarraVida.Value = value; }
+        }
+
+        public double energia
+        {
+            get { return this.BarraEnergia.Value; }
+            set { this.BarraEnergia.Value = value; }
+        }
+
+        double iPokemon.Vida { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        double iPokemon.Energia { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        string iPokemon.Nombre { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        string iPokemon.Categoría { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        string iPokemon.Tipo { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        double iPokemon.Altura { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        double iPokemon.Peso { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        string iPokemon.Evolucion { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        string iPokemon.Descripcion { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public MimikyuCBM()
+        {
+            this.InitializeComponent();
+            this.IsTabStop = true;
+        }
+
+        private void ControlTeclas(object sender, KeyRoutedEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Windows.System.VirtualKey.Number1:
+                    Storyboard sb = (Storyboard)this.Resources["StMovimiento"];
+                    sb.Begin();
+                    break;
+                case Windows.System.VirtualKey.Number2:
+                    Storyboard sb1 = (Storyboard)this.Resources["StGarra"];
+                    sb1.Begin();
+                    MediaPlayer mpSonidosGarra = new MediaPlayer();
+                    mpSonidosGarra.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///AssetsMimikyuCBM/garra.mp3"));
+                    mpSonidosGarra.Play();
+                    break;
+                case Windows.System.VirtualKey.Number3:
+                    Storyboard sb2 = (Storyboard)this.Resources["StBola"];
+                    sb2.Begin();
+                    MediaPlayer mpSonidosBola = new MediaPlayer();
+                    mpSonidosBola.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///AssetsMimikyuCBM/bola.mp3"));
+                    mpSonidosBola.Play();
+                    break;
+                case Windows.System.VirtualKey.Number4:
+                    Storyboard sb3 = (Storyboard)this.Resources["StDefensa"];
+                    sb3.Begin();
+                    MediaPlayer mpSonidosDefensa = new MediaPlayer();
+                    mpSonidosDefensa.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///AssetsMimikyuCBM/escudo.mp3"));
+                    mpSonidosDefensa.Play();
+                    break;
+                case Windows.System.VirtualKey.Number5:
+                    Storyboard sb4 = (Storyboard)this.Resources["StRecuperacion"];
+                    sb4.Begin();
+                    MediaPlayer mpSonidosRecuperacion = new MediaPlayer();
+                    mpSonidosRecuperacion.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///AssetsMimikyuCBM/recuperacion.mp3"));
+                    mpSonidosRecuperacion.Play();
+                    break;
+                case Windows.System.VirtualKey.Number6:
+                    Storyboard sb5 = (Storyboard)this.Resources["StHerido"];
+                    sb5.Begin();
+                    break;
+                case Windows.System.VirtualKey.Number7:
+                    Storyboard sb6 = (Storyboard)this.Resources["StCansado"];
+                    sb6.Begin();
+                    break;
+                case Windows.System.VirtualKey.Number8:
+                    Storyboard sb7 = (Storyboard)this.Resources["StDerrotado"];
+                    sb7.Begin();
+                    break;
+            }
+        }
+
+        private void usarPocimaVida(object sender, PointerRoutedEventArgs e)
+        {
+            dtTimeVida = new DispatcherTimer();
+            dtTimeVida.Interval = TimeSpan.FromMilliseconds(100);
+            dtTimeVida.Tick += incrementarVida;
+            dtTimeVida.Start();
+            this.ImagenPocionRoja.Visibility = Visibility.Collapsed;
+        }
+
+        private void incrementarVida(object sender, object e)
+        {
+            if (BarraVida.Value < 100)
+            {
+                BarraVida.Value++;
+            }
+            else
+            {
+                this.dtTimeVida.Stop();
+            }
+        }
+
+        private void usarPocionEnergia(object sender, PointerRoutedEventArgs e)
+        {
+            dtTimeEergia = new DispatcherTimer();
+            dtTimeEergia.Interval = TimeSpan.FromMilliseconds(100);
+            dtTimeEergia.Tick += incrementarEnergia;
+            dtTimeEergia.Start();
+            this.ImagenPocionAmarilla.Visibility = Visibility.Collapsed;
+        }
+
+        private void incrementarEnergia(object sender, object e)
+        {
+            if (BarraEnergia.Value < 100)
+            {
+                BarraEnergia.Value++;
+            }
+            else
+            {
+                this.dtTimeEergia.Stop();
+            }
+        }
+
+        public void verFondo(bool ver)
+        {
+            if (ver)
+            {
+                FondoPantalla.Opacity = 100;
+            }
+            else
+            {
+                FondoPantalla.Opacity = 0;
+            }
+        }
+
+        public void verFilaVida(bool ver)
+        {
+            if (ver)
+            {
+                BarraVida.Opacity = 100;
+            }
+            else
+            {
+                BarraVida.Opacity = 0;
+            }
+        }
+
+        public void verFilaEnergia(bool ver)
+        {
+            if (ver)
+            {
+                BarraEnergia.Opacity = 100;
+            }
+            else
+            {
+                BarraEnergia.Opacity = 0;
+            }
+        }
+
+        public void verPocionVida(bool ver)
+        {
+            if (ver)
+            {
+                ImagenPocionRoja.Opacity = 100;
+            }
+            else
+            {
+                ImagenPocionRoja.Opacity = 0;
+            }
+        }
+
+        public void verPocionEnergia(bool ver)
+        {
+            if (ver)
+            {
+                ImagenPocionAmarilla.Opacity = 100;
+            }
+            else
+            {
+                ImagenPocionAmarilla.Opacity = 0;
+            }
+        }
+
+        public void verNombre(bool ver)
+        {
+            if (ver)
+            {
+                NombrePokemon.Opacity = 100;
+            }
+            else
+            {
+                NombrePokemon.Opacity = 0;
+            }
+        }
+
+        public void verEscudo(bool ver)
+        {
+            if (ver)
+            {
+                SombraCabeza.Opacity = 100;
+                SombraCola.Opacity = 100;
+                SombraCuerpo.Opacity = 100;
+                SombraOrejaDrc.Opacity = 100;
+                SombraOrejaIzq.Opacity = 100;
+            }
+            else
+            {
+                SombraCabeza.Opacity = 0;
+                SombraCola.Opacity = 0;
+                SombraCuerpo.Opacity = 0;
+                SombraOrejaDrc.Opacity = 0;
+                SombraOrejaIzq.Opacity = 0;
+            }
+        }
+
+        public void activarAniIdle(bool activar)
+        {
+            StMovimiento.Begin();
+        }
+
+        public void animacionAtaqueFlojo()
+        {
+            StGarra.Begin();
+            MediaPlayer mpSonidosGarra = new MediaPlayer();
+            mpSonidosGarra.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///AssetsMimikyuCBM/garra.mp3"));
+            mpSonidosGarra.Play();
+        }
+
+        public void animacionAtaqueFuerte()
+        {
+            StBola.Begin();
+            MediaPlayer mpSonidosBola = new MediaPlayer();
+            mpSonidosBola.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///AssetsMimikyuCBM/bola.mp3"));
+            mpSonidosBola.Play();
+        }
+
+        public void animacionDefensa()
+        {
+            StDefensa.Begin();
+            MediaPlayer mpSonidosDefensa = new MediaPlayer();
+            mpSonidosDefensa.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///AssetsMimikyuCBM/escudo.mp3"));
+            mpSonidosDefensa.Play();
+        }
+
+        public void animacionDescasar()
+        {
+            StRecuperacion.Begin();
+            MediaPlayer mpSonidosRecuperacion = new MediaPlayer();
+            mpSonidosRecuperacion.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///AssetsMimikyuCBM/recuperacion.mp3"));
+            mpSonidosRecuperacion.Play();
+        }
+
+        public void animacionCansado()
+        {
+            StCansado.Begin();
+        }
+
+        public void animacionNoCansado()
+        {
+            StCansado.Stop();
+        }
+
+        public void animacionHerido()
+        {
+            StHerido.Begin();
+        }
+
+        public void animacionNoHerido()
+        {
+            StHerido.Stop();
+        }
+
+        public void animacionDerrota()
+        {
+            StDerrotado.Begin();
+        }
+    }
+}
